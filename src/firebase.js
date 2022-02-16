06fd0512bff65fd1a898912df93ac47cd7061b6f
AleksandrKrivonos <A.KRIVONOS@didlogic.com>
2022-02-16 14:37:27 +0300

    configure vite template

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAUGgEtiUa43xvvfPPx12Buahl-9USopc8",
+    authDomain: "netflix-db140.firebaseapp.com",
+    projectId: "netflix-db140",
+    storageBucket: "netflix-db140.appspot.com",
+    messagingSenderId: "854882318161",
+    appId: "1:854882318161:web:b7310bc29593ef086b4570"
+};
